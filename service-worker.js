39bf4d5e2e9682d3d2b832b9669c7a92b52e80d5
Lars Verhaeghe <148888518+larsv2801@users.noreply.github.com>
2026-06-30 19:36:24 +0200

    Modify fetch event handling in service worker
    Added a condition to fetch requests for specific URLs.

--- service-worker.js
+++ service-worker.js
@@ -13,6 +13,12 @@ self.addEventListener('install', (event) => {
 });
 
 self.addEventListener('fetch', (event) => {
+  const url = new URL(event.request.url);
+
+  if (url.pathname === '/ytadfree' && url.searchParams.has('url')) {
+    return event.respondWith(fetch(event.request));
+  }
+
   event.respondWith(
     caches.match(event.request).then((response) => {
       return response || fetch(event.request);
